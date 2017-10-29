a08beba1b2d66208d69468841cfc5542accdceaf
companyrad <31884631+companyrad@users.noreply.github.com>
2017-10-29 09:28:03 -0700

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,13 +1,13 @@
 do
 local _ = {
 		TOKEN = 'TOKEN', 
-		PayIRKey = "4fa9e7b977a298dd964763016ef772ac",
+		PayIRKey = "436335797:AAFH0v0e2-freqZR-j9DroVUC9ln9VmztFE",
 		APITOKEN = "436335797:AAFH0v0e2-freqZR-j9DroVUC9ln9VmztFE", -- GET From https://api.reloadlife.me/#Code
 		MainSudo = 342293523, -- Main SUDO
 		BotID = 433435731, -- CLI BOT ID
 		Sudo = {
 			342293523, -- SUDOS
-			0 -- API BOT ID
+			436335797 -- API BOT ID
 		},
 		Plugins = {
 			'Checker', -- MUST BE FIRST FOR SPEED :)
